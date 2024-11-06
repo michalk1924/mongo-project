44e8a9571edd5b228ec72f1c92c0b3431c49c9cd
michalk1924 <michalk1924@gmail.com>
2024-11-06 09:20:53 +0200

    add config to gitgnore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 "use client"
 
-import { useEffect, useState } from "react";
-import { getAllDocuments, connectDatabase } from "@/services/mongo";
-import carsService from "@/services/cars";
-import './style.css'; 
-
-
 export default function Home() {
     <div/>
 }
